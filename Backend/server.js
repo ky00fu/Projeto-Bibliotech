30d110bd4de1366a6d9ba38917473b8e494cb323
ky00fu <kirsycu@gmail.com>
2023-10-25 08:19:33 -0300

    created branch outubro-bd-be

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,10 @@ app.use(express.json())
 app.use(cors())
 app.use('/', router)
 
-app.listen(2000, () => {
-    console.info('[Server running on 2000]')
+// app.listen(2000, () => {
+//     console.info('[Server running on 2000]')
+// } )
+
+app.listen(3000, () => {
+    console.info('[Server running on 3000]')
 } )
