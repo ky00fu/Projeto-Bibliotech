63190099cfb783d40b405cf4d8d52468ff97ebfa
ky00fu <kirsycu@gmail.com>
2023-11-08 08:41:15 -0300

    update routes

--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-const Cliente = require('../controllers/cliente');
 
+
+const Cliente = require('../controllers/cliente');
 router.post('/cliente/registro', Cliente.criar);
 router.get('/cliente/listar', Cliente.listar);
 router.get('/cliente/listar/:id', Cliente.listar);
 
-const Adm = require('../controllers/adm');
 
+
+const Adm = require('../controllers/adm');
 router.get('/adm/listar', Adm.listar);
 
-const Emprestimo = require('../controllers/emprestimo');
 
+
+const Emprestimo = require('../controllers/emprestimo');
 router.post('/emprestimo/criar', Emprestimo.criar);
 router.get('/emprestimo/listar', Emprestimo.listar);
 router.get('/emprestimo/listar/:id_cliente', Emprestimo.listar);
 router.put('/emprestimo/alterar/:id', Emprestimo.alterar);
 router.delete('/emprestimo/excluir/:id', Emprestimo.excluir);
+
+
 
 module.exports = router
