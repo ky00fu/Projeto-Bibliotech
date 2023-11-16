1a4c08ec79820f2165b7c5a9d5f80ad1be38df54
ky00fu <kirsycu@gmail.com>
2023-11-16 18:41:17 -0300

    página cadastro

--- Frontend/Pag inicial/Cadastro/script.js
+++ Frontend/Pag inicial/Cadastro/script.js
@@ -2,15 +2,23 @@ const url = "http://localhost:3000"
 
 const cadastro = document.querySelector('.cadastro');
 const mensagem = document.querySelector('#msg');
-// const msgSucesso = document.querySelector('#msgSucesso');
 
 cadastro.addEventListener('submit', function (e) {
     e.preventDefault();
 
+    const inpNome = document.getElementById('nome');
+    const inpEmail = document.getElementById('email');
+    const inpSenha = document.getElementById('senha');
+
+    if (inpSenha.value.length < 8) {
+        mensagem.textContent = "A senha deve ter pelo menos 8 caracteres.";
+        return; // Stop execution if the password is too short
+    }
+
     const dados = {
-        nome: cadastro.nome.value,
-        email: cadastro.email.value,
-        senha: cadastro.senha.value
+        nome: inpNome.value,
+        email: inpEmail.value,
+        senha: inpSenha.value
     }
 
     const options = {
@@ -21,18 +29,14 @@ cadastro.addEventListener('submit', function (e) {
 
     fetch(url + '/cliente', options)
         .then((resp) => {
-            let inpNome = document.getElementById('nome')
-            let inpEmail = document.getElementById('email')
-            let inpSenha = document.getElementById('senha')
-
             if (inpNome.value === "" || inpEmail.value === "" || inpSenha.value === "") {
-                mensagem.textContent = "Preencha os campos abaixo"
+                mensagem.textContent = "Preencha todos os campos abaixo."
             } else if (inpNome.value && inpEmail.value && inpSenha.value) {
                 if (resp.length > 0) {
-                    mensagem.textContent = "Usuário não criado"
+                    mensagem.textContent = "Usuário não criado.";
                 } else {
-                    mensagem.classList.add("sucesso")
-                    mensagem.textContent = "Usuário criado com sucesso"
+                    mensagem.classList.add("sucesso");
+                    mensagem.textContent = "Usuário criado com sucesso.";
                 }
             }
         })
