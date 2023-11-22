b4c8f1f7d96235b036c4a441d4d7c3939c8f24dc
ky00fu <kirsycu@gmail.com>
2023-11-22 16:32:24 -0300

    página login cliente

--- Frontend/Pag inicial/Login/Cliente/script.js
+++ Frontend/Pag inicial/Login/Cliente/script.js
@@ -1,43 +1,47 @@
-const url = "http://localhost:3000"
+const url = "http://localhost:3000";
 
-const acesso = document.querySelector('.acesso');
-const msgErro = document.querySelector('#msgErro');
+const acesso = document.querySelector(".acesso");
+const msgErro = document.querySelector("#msgErro");
 
-acesso.addEventListener('submit', function (e) {
-    e.preventDefault();
+acesso.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const dados = {
-        email: acesso.email.value,
-        senha: acesso.senha.value
-    }
+  const dados = {
+    email: acesso.email.value,
+    senha: acesso.senha.value,
+  };
 
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(dados)
-    }
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(dados),
+  };
 
-    fetch(url + '/cliente', options)
-        .then((resp) => {
-            if (resp.status !== 202) {
-                msgErro.textContent = "E-mail ou senha incorreta"
-            } else {
-                return resp.json()
-            }
-        })
-        .then((resp) => {
-            let inpEmail = document.getElementById('email')
-            let inpSenha = document.getElementById('senha')
+  fetch(url + "/cliente", options)
+    .then((resp) => {
+      if (resp.status !== 202) {
+        msgErro.textContent = "E-mail ou senha incorreta";
+      } else {
+        return resp.json();
+      }
+    })
+    .then((resp) => {
+      let inpEmail = document.getElementById("email");
+      let inpSenha = document.getElementById("senha");
 
-            if (inpEmail.value === "" || inpSenha.value === "") {
-                msgErro.textContent = "Preencha os campos abaixo"
-            } else if (inpEmail.value && inpSenha.value) {
-                if (resp.length > 0) {
-                    window.localStorage.setItem('dados', JSON.stringify(resp[0]))
-                    window.location.href = `../../../Pag cliente/index.html`
-                } else {
-                    msgErro.textContent = "Usuário não encontrado"
-                }
-            }
-        })
+      if (inpEmail.value === "" || inpSenha.value === "") {
+        msgErro.textContent = "Preencha os campos abaixo";
+      } else if (inpEmail.value && inpSenha.value) {
+        if (resp.length > 0) {
+          window.localStorage.setItem("dados", JSON.stringify(resp[0]));
+          window.location.href = `../../../Pag cliente/index.html`;
+        } else {
+          msgErro.textContent = "Usuário não encontrado";
+        }
+      }
+    });
 });
+
+function redirectHome() {
+  window.location.href = `../../Entrada/index.html`;
+}
