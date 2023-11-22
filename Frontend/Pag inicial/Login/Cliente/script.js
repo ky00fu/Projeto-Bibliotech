86ac736668d96640a15259c97d10813268bf39e3
ky00fu <kirsycu@gmail.com>
2023-11-22 17:28:28 -0300

    página login cliente

--- Frontend/Pag inicial/Login/Cliente/script.js
+++ Frontend/Pag inicial/Login/Cliente/script.js
@@ -1,7 +1,8 @@
 const url = "http://localhost:3000";
 
 const acesso = document.querySelector(".acesso");
-const msgErro = document.querySelector("#msgErro");
+const msgDiv = document.querySelector(".erro");
+const mensagem = document.querySelector("#msgErro");
 
 acesso.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -20,7 +21,13 @@ acesso.addEventListener("submit", function (e) {
   fetch(url + "/cliente", options)
     .then((resp) => {
       if (resp.status !== 202) {
-        msgErro.textContent = "E-mail ou senha incorreta";
+        msgDiv.offsetWidth;
+        msgDiv.classList.remove("show");
+
+        mensagem.innerHTML = "E-mail ou senha incorreta";
+
+        msgDiv.offsetWidth;
+        msgDiv.classList.add("show");
       } else {
         return resp.json();
       }
@@ -30,13 +37,25 @@ acesso.addEventListener("submit", function (e) {
       let inpSenha = document.getElementById("senha");
 
       if (inpEmail.value === "" || inpSenha.value === "") {
-        msgErro.textContent = "Preencha os campos abaixo";
+        msgDiv.offsetWidth;
+        msgDiv.classList.remove("show");
+
+        mensagem.innerHTML = "Preencha os campos abaixo";
+
+        msgDiv.offsetWidth;
+        msgDiv.classList.add("show");
       } else if (inpEmail.value && inpSenha.value) {
         if (resp.length > 0) {
           window.localStorage.setItem("dados", JSON.stringify(resp[0]));
           window.location.href = `../../../Pag cliente/index.html`;
         } else {
-          msgErro.textContent = "Usuário não encontrado";
+          msgDiv.offsetWidth;
+          msgDiv.classList.remove("show");
+
+          mensagem.textContent = "Usuário não encontrado";
+
+          msgDiv.offsetWidth;
+          msgDiv.classList.add("show");
         }
       }
     });
